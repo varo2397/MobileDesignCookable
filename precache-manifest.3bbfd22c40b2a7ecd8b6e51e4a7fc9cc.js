dcbd4ca618bff00f8d3fd6583f215f7d1ffc8436
Alvaro Castro <varo2397>
2020-04-11 18:57:27 -0600

    Updates

---
+++ precache-manifest.3bbfd22c40b2a7ecd8b6e51e4a7fc9cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "626a5261d0d7025399e08a4bb9083c0f",
+    "url": "/MobileDesignCookable/index.html"
+  },
+  {
+    "revision": "f53e07760b1fe194dbc1",
+    "url": "/MobileDesignCookable/static/css/main.6505ec3d.chunk.css"
+  },
+  {
+    "revision": "7eaedaabddc8413340fb",
+    "url": "/MobileDesignCookable/static/js/2.e845ed85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MobileDesignCookable/static/js/2.e845ed85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f53e07760b1fe194dbc1",
+    "url": "/MobileDesignCookable/static/js/main.0140cf12.chunk.js"
+  },
+  {
+    "revision": "146123b9d6810234c830",
+    "url": "/MobileDesignCookable/static/js/runtime-main.db83ef50.js"
+  }
+]);
