c0a55f136e1f2b7b34d86333dfd0cdaa161380d2
Alvaro Castro <varo2397>
2020-04-13 01:30:52 -0600

    FIxed progress ba bug

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -62,7 +62,7 @@ class ProgressBar extends Component {
 
                                 <div className="d-flex flex-column pr-2" style={{ width: '20%' }}>
 
-                                    <p className="text-white align-self-center progress-text" style={{marginBottom: -2}}><i
+                                    <p className="text-white align-self-center progress-text" style={{marginBottom: isPortrait ? -2: ''}}><i
                                         className="fas fa-chevron-double-down current-step-icon-details"></i></p>
                                     <div className="d-flex flex-row">
                                         <div className="progress-default progress-process"></div>
