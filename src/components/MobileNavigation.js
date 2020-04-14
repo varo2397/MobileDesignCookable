b4a6e387703255c5046dd121c7de3d520dfcf760
Alvaro Castro <varo2397>
2020-04-14 01:40:35 -0600

    Fixed several issues with media queries

--- src/components/MobileNavigation.js
+++ src/components/MobileNavigation.js
@@ -11,7 +11,7 @@ class MobileNavigation extends Component {
     render() {
         const { toggleNavigation } = this.props;
         return (
-            <div className="absolute d-flex flex-row" onClick={() => toggleNavigation()} style={{ width: '100vw', height: '100vh', backgroundColor: 'transparent !important', zIndex: 1000 }}>
+            <div className="absolute d-flex flex-row" onClick={() => toggleNavigation()} style={{ width: '100vw', height: '100%', backgroundColor: 'transparent !important', zIndex: 1000 }}>
                 <div className="h-100 w-50 d-flex flex-row justify-content-end align-items-center ">
                     <div className="d-flex flex-row justify-content-end align-items-center text-white pr-5" onClick={this.onClickNavigationButton}>
                         <span className="pr-4">PREVIEW VIDEO</span>
--- src/components/Portrait/Layout.js
+++ src/components/Portrait/Layout.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { RecipeTitle, ProgressBar, OptinsBar } from '../';
+import withMobile from '../../hoc/withMobile';
 
 class Layout extends Component {
     render() {
@@ -14,14 +15,15 @@ class Layout extends Component {
             Let wings dry uncovered for an hour(or up to overnight in the fridge).
             `,
             name = "Crispiest Juiciest Oven Baked Wings",
-            author = "- by Grace Choi"
+            author = "- by Grace Choi",
+            isMobile
         } = this.props;
 
         return (
             <div className="w-100" style={{ backgroundColor: 'black', height: '100vh', overflowY: 'scroll', overflowX: 'hidden' }}>
                 <div className="relative">
                     <div className="gradient-top" style={{ height: '20%', backgroundImage: 'linear-gradient(rgba(0,0,0,0.8), rgba(0,0,0,0.01))' }}></div>
-                    <video loop muted autoPlay src={process.env.PUBLIC_URL + "/Beer_Pour_Videvo.mp4"} style={{ width: '100vw', height: '30vh', objectFit: 'fill' }}></video>
+                    <video loop muted autoPlay src={process.env.PUBLIC_URL + "/Beer_Pour_Videvo.mp4"} style={{ width: '100vw', height: '30vh', objectFit: 'contain' }}></video>
                     <div className="absolute" style={{ top: 0, left: 10 }}>
                         <RecipeTitle
                             name={name}
@@ -30,10 +32,10 @@ class Layout extends Component {
                     </div>
                     <div className="absolute gradient-bottom" style={{ bottom: 0, height: '20%', width: '100%' }}></div>
                 </div>
-                <div className="w-100 pl-2 pr-2">
+                <div className="w-100 pl-2 pr-2" style={{paddingLeft: isMobile ? '' : '1.5rem !important', paddingRight: isMobile ? '' : '1.5rem !important'}}>
                     <ProgressBar />
                 </div>
-                <div className="d-flex flex-column pl-2 pr-2" >
+                <div className="d-flex flex-column pl-2 pr-2"  >
                     <div className="d-flex flex-row">
                         <p className="text-white instructional-title">{instructionsTitle} &nbsp;
                                             <span>{instructionsStep}</span>
@@ -53,4 +55,4 @@ class Layout extends Component {
     }
 }
 
-export default Layout
+export default withMobile(Layout); 
