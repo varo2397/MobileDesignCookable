da07dce05d2f50d8f991de7ad7619259a103709c
Alvaro Castro <varo2397>
2020-04-20 18:34:55 -0600

    Fixed several bugs

--- src/components/MobileNavigation.js
+++ src/components/MobileNavigation.js
@@ -14,14 +14,14 @@ class MobileNavigation extends Component {
             <div className="absolute d-flex flex-row" onClick={() => toggleNavigation()} style={{ width: '100vw', height: '100%', backgroundColor: 'transparent !important', zIndex: 1000 }}>
                 <div className="h-100 w-50 d-flex flex-row justify-content-end align-items-center ">
                     <div className="d-flex flex-row justify-content-end align-items-center text-white pr-5" onClick={this.onClickNavigationButton}>
-                        <span className="pr-4">PREVIEW VIDEO</span>
                         <i className="far fa-angle-left" style={{ fontSize: 30 }}></i>
+                        <span className="pl-4">PREVIEW VIDEO</span>
                     </div>
                 </div>
                 <div className="h-100 w-50 d-flex flex-row justify-content-start align-items-center">
                     <div className="d-flex flex-row justify-content-start align-items-center text-white pl-5" onClick={this.onClickNavigationButton}>
+                        <span className="pr-4">PREVIEW VIDEO</span>
                         <i className="far fa-angle-right" style={{ fontSize: 30 }}></i>
-                        <span className="pl-4">PREVIEW VIDEO</span>
                     </div>
                 </div>
             </div>
